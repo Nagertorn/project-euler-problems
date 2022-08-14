a220b73f1467640df073ae492dd5f58f9f0902cc
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-08-14 19:40:04 +0200

    Remove bad solution

--- p5-smallest-multiple/remove_impostors.py
+++
@@ -1,2 +0,0 @@
-def remove_impostors(range_of_numbers: list) -> list:
-    return range_of_numbers
--- p5-smallest-multiple/solution_by_hand.py
+++
@@ -1,7 +0,0 @@
-from mult_eachother_in import mult_eachother_in
-
-
-def solution_by_hand(valid_nums: list) -> int:
-    solution1 = mult_eachother_in(valid_nums)
-
-    return solution1
